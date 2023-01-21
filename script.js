80b3bbf7ad7442586ec088764aacd7a9d454de1a
Michael Hamilton <michael.ts.hamilton@gmail.com>
2023-01-21 01:16:32 -0600

    Created HTML, JS and CSS files

---
+++ script.js
@@ -0,0 +1,3 @@
+// build a function knightMoves that shows the shortest possible way to get from one square to another on an 8x8 grid by outputting all square the knight will stop on along the way. You can think of the board as having 2-dimensional coordinates. Your function would look something like this: knightMoves([0,0], [1,2]) == [[0,0], [1,2]]
+
+// The knight can move to any square that is two squares horizontally and one square vertically, or two squares vertically and one square horizontally away from it. The complete move therefore looks like the letter L.
